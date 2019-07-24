68ee5b90313ecba98265c46c5a1951b651349ea7
eaNewton <emmett.newton@praxent.com>
2019-07-24 12:18:24 -0500

    add extractFromAddress function

--- app.js
+++ app.js
@@ -6,3 +6,16 @@ const addresses = require('./data/addresses_test.js').addresses;
 geobatch = new GeoBatch({
   apiKey: process.env.API_KEY,
 });
+
+function extractFromAddress(components, type) {
+  if (!components) {
+    console.log("No components found.");
+    return;
+  }
+
+  return components.filter(
+    (component) => component.type.indexOf(type) === 0
+  ).map(
+    (item) => item.long_name
+  ).pop() || null
+}
