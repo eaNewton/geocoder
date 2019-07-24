37f3fb9f9c95473983dd422a44b4357151120654
eaNewton <emmett.newton@praxent.com>
2019-07-24 11:58:17 -0500

    add require statements, import api key

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 require('dotenv').config({path: __dirname + '/.env'});
+
+const GeoBatch = require('geobatch');
+const addresses = require('./data/addresses_test.js').addresses;
+
+geobatch = new GeoBatch({
+  apiKey: process.env.API_KEY,
+});
