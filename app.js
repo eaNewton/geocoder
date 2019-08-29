536b4fbf295f1aa45ffb7a835eadfc2c8e18e25f
eaNewton <emmett.newton@praxent.com>
2019-08-29 16:32:46 -0500

    rough draft one-pager

--- app.js
+++ app.js
@@ -19,3 +19,37 @@ function extractFromAddress(components, type) {
     (item) => item.long_name
   ).pop() || null
 }
+
+geoBatch.geocode(addresses)
+  .on('data', function(data) {
+    const subpremise = extractFromAddress(data.results.address_components, 'subpremise');
+    const street_number = extractFromAddress(data.result.address_components, 'street_number');
+    const route = extractFromAddress(data.result.address_components, 'route');
+    const locality = extractFromAddress(data.result.address_components, 'locality');
+    const admin_area_1 = extractFromAddress(data.result.address_components, 'administrative_area_level_1');
+    const zip = extractFromAddress(data.result.address_components, 'postal_code');
+
+    const addressComponents = {};
+    addressComponents['subpremise'] = subpremise;
+    addressComponents['street_number'] = street_number;
+    addressComponents['route'] = route;
+    addressComponents['locality'] = locality;
+    addressComponents['admin_area_1'] = admin_area_1; 
+    addressComponents['zip'] = zip;
+
+    const dataArray = {};
+    dataArray['full_input'] = data.address;
+    dataArray['formatted_address'] = data.result.formatted_address;
+    dataArray['address_components'] = addressComponents;
+    dataArray['lat'] = data.result.geometry.location.lat;
+    dataArray['lng'] = data.result.geometry.location.lng;
+    dataArray['place_id'] = data.result.place_id;
+    dataArray['types'] = data.result.types;
+
+    const dataArrayJSON = JSON.stringify(dataArray);
+
+    console.log(dataArrayJSON);
+  })
+  .on('end', function() {
+    console.log('Finished!');
+  });
